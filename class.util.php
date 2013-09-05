8d8bd855a71b330e0fe3ed980ae17caaf3810a66
Andr3as <andr3as@me>
2013-09-05 13:02:04 +0200

    Copyright added

--- class.util.php
+++ class.util.php
@@ -1,5 +1,9 @@
 <?php
-
+/*
+ * Copyright (c) Codiad & Andr3as, distributed
+ * as-is and without warranty under the MIT License. 
+ * See [root]/license.md for more information. This information must remain intact.
+ */
     class util {
         
         static public function getWorkspacePath($path) {
