d0bf9655922225c583a9f148dd395ee58323bb2f
Andr3as <andr3as@me>
2014-05-26 18:28:45 +0200

    Easy setup added

--- dialog.php
+++ dialog.php
@@ -3,7 +3,7 @@
     as-is and without warranty under the MIT License. 
     See [root]/license.md for more information. This information must remain intact.
 -->
-<form>
+<form id="permissions_dialog">
     <?php
         error_reporting(0);
         
@@ -20,11 +20,35 @@
         echo '
             <p>Change permissions</p>
             <input id="permissions" placeholder="'.$perm.'" type="text">
-            <button onclick="codiad.Permissions.change(); return false;">Change</button>
-            <button onclick="codiad.modal.unload(); return false;">Close</button>
         ';
     ?>
+    <div class="more_control"><span class="item-icon icon-right-open-big"> Easy setup</span></div>
+    <div class="more">
+		<table>
+			<tr class="owner">
+				<td>Owner</td>
+				<td><input type="checkbox" class="owner r"> R</td>
+				<td><input type="checkbox" class="owner w"> W</td>
+				<td><input type="checkbox" class="owner x"> X</td>
+			</tr>
+			<tr class="group">
+				<td>Group</td>
+				<td><input type="checkbox" class="group r"> R</td>
+				<td><input type="checkbox" class="group w"> W</td>
+				<td><input type="checkbox" class="group x"> X</td>
+			</tr>
+			<tr class="other">
+				<td>Other</td>
+				<td><input type="checkbox" class="other r"> R</td>
+				<td><input type="checkbox" class="other w"> W</td>
+				<td><input type="checkbox" class="other x"> X</td>
+			</tr>
+		</table>
+    </div>
+    <button onclick="codiad.Permissions.change(); return false;">Change</button>
+    <button onclick="codiad.modal.unload(); return false;">Close</button>
     <script>
+        $('.more').hide();
         $('#permissions').focus();
     </script>
 </form>
--- init.js
+++ init.js
@@ -21,6 +21,24 @@
         file    : "",
         
         init: function() {
+			var _this = this;
+			$('.more_control').live('click', function(e){
+				$('.more').toggle(0, function(){
+					$('.more_control span').removeClass('icon-right-open-big');
+					$('.more_control span').removeClass('icon-down-open-big');
+					if ($('.more').is(':visible')) {
+						$('.more_control span').addClass('icon-down-open-big');
+					} else {
+						$('.more_control span').addClass('icon-right-open-big');
+					}
+				});
+			});
+			$('.more input:checkbox').live('change', function(e){
+				_this.calculateRights();
+			});
+			$('#permissions').live('input keyup', function(e){
+				_this.calculateBoxes($('#permissions').val());
+			});
         },
         
         //////////////////////////////////////////////////////////
@@ -52,6 +70,107 @@
                     codiad.message.success(data.message);
                 }
             });
+        },
+        
+        //////////////////////////////////////////////////////////
+        //
+        //  Calculate rights
+        //
+		//////////////////////////////////////////////////////////
+        calculateRights: function() {
+			var sum = 0, _this = this;
+			$('.more input:checkbox').each(function(i, item){
+				var factor = 1, right = 1;
+				if (_this.isChecked(item)) {
+					if ($(item).hasClass('owner')) {
+						factor = 100;
+					} else if ($(item).hasClass('group')) {
+						factor = 10;
+					}
+					
+					if ($(item).hasClass('r')) {
+						right = 4;
+					} else if($(item).hasClass('w')) {
+						right = 2;
+					}
+					sum += factor * right;
+				}
+			});
+			var obj = $('.more input:checkbox');
+			if (!this.isChecked(obj[0]) && !this.isChecked(obj[1]) && !this.isChecked(obj[2])) {
+				sum = '0' + sum;
+				if (!this.isChecked(obj[3]) && !this.isChecked(obj[4]) && !this.isChecked(obj[5])) {
+					sum = '0' + sum;
+				}
+			}
+			$('#permissions').val(sum);
+        },
+        
+        //////////////////////////////////////////////////////////
+        //
+        //  Calculate checkboxes
+        //
+        //  Parameter:
+        //
+        //  rights - {string} - File rights
+        //
+		//////////////////////////////////////////////////////////
+        calculateBoxes: function(rights) {
+            $('.more input:checkbox').removeAttr('checked');
+            var fn = function(right, selector) {
+				if (right == '1' || right == '3' || right == '5' || right == '7') {
+					this.selectCheckbox(selector + ' .x');
+				}
+				if (right == '2' || right == '3' || right == '6' || right == '7') {
+					this.selectCheckbox(selector + ' .w');
+				}
+				if (right == '4' || right == '5' || right == '6' || right == '7') {
+					this.selectCheckbox(selector + ' .r');
+				}
+            }.bind(this);
+            if (rights[0] === '0' && rights.length > 3) {
+				rights = rights.substring(1);
+            }
+            if (rights.length < 1) {
+				rights = '000';
+            } else if (rights.length < 2) {
+				rights += '00';
+            } else {
+				rights += '0';
+            }
+            fn(rights[0], ".owner");
+            fn(rights[1], ".group");
+            fn(rights[2], ".other");
+        },
+        
+        //////////////////////////////////////////////////////////
+        //
+        //  Check if checkbox is checked
+        //
+        //  Parameter:
+        //
+        //  item - {object} - jQuery object
+        //
+		//////////////////////////////////////////////////////////
+        isChecked: function(item) {
+			if ($(item).attr('checked') == "checked") {
+				return true;
+            } else {
+				return false;
+            }
+        },
+        
+        //////////////////////////////////////////////////////////
+        //
+        //  Select checkbox
+        //
+        //  Parameter:
+        //
+        //  selector - {string} - Selector of checkbox(s)
+        //
+		//////////////////////////////////////////////////////////
+        selectCheckbox: function(selector) {
+			$('.more ' + selector).attr('checked', 'checked');
         }
         
     };
